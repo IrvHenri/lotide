23d16024d3efc7ddcee4517a0b06fe44d34389e9
Irving Henriquez <henriquezirv@gmail.com>
2021-04-28 19:22:59 +0000

    completed countLetters assignment

---
+++ countLetters.js
@@ -0,0 +1,33 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+// should receive a string input
+// return a count of each of the letters in that sentence.
+let string = "Irving";
+let string2 = "Hello World";
+
+const countLetters = function(string) {
+  let lowerCaseString = string.split(" ").join("").toLowerCase();
+
+  let results = {};
+  for (let char of lowerCaseString) {
+    if (results[char]) {
+      results[char] += 1;
+    } else {
+      results[char] = 1;
+    }
+  }
+
+  return results;
+};
+
+let output = countLetters(string);
+let output2 = countLetters(string2);
+
+assertEqual(output["i"], 2);
+assertEqual(output2["l"], 3);
