60d1bb4f443b79df7632df949d9510eafd5c9559
Irving Henriquez <henriquezirv@gmail.com>
2021-04-29 18:24:27 +0000

    fixed takeUntil.js formatting

--- takeUntil.js
+++ takeUntil.js
@@ -1,6 +1,6 @@
 // TESTING
 
-const eqArrays = function (arrOne, arrTwo) {
+const eqArrays = function(arrOne, arrTwo) {
   if (arrOne.length !== arrTwo.length) {
     return false;
   }
@@ -12,7 +12,7 @@ const eqArrays = function (arrOne, arrTwo) {
   }
   return true;
 };
-const assertArrayEqual = function (arrayOne, arrayTwo) {
+const assertArrayEqual = function(arrayOne, arrayTwo) {
   if (eqArrays(arrayOne, arrayTwo)) {
     console.log(`✅✅✅ Assertion Passed: [${arrayOne}] === [${arrayTwo}]`);
   } else {
@@ -22,14 +22,14 @@ const assertArrayEqual = function (arrayOne, arrayTwo) {
 
 // TAKE UNTIL FUNCTION
 
-/* 
+/*
 It will take in two parameters:
 The array to work with
 The callback (which Lodash calls "predicate")
 The function will return a "slice of the array with elements taken from the beginning." It should keep going until the callback/predicate returns a truthy value.
 */
 
-const takeUntil = function (array, callback) {
+const takeUntil = function(array, callback) {
   // ...
   let result = [];
 
